40355d5e89f4f22f2c276201a92c4425d14fbbd3
Stephan Nielsen <stephannielsen@gmail.com>
2018-09-16 15:04:19 +0200

    Initial app setup and vscode setup.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UkoaConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
